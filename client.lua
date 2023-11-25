e0a8cd1715c6eb44bb09dfece9a4730590715923
Goat_Rico <85892640+TheGoatRico@users.noreply.github.com>
2023-11-24 22:32:40 -0600

    Refactor client.lua

--- client.lua
+++ client.lua
@@ -3,102 +3,93 @@
 --------------------------------
 
 --- Config ---
+local Config = {
+    reviveTimer = 10,  -- Time to wait before allowing revive (in seconds)
+    respawnTimer = 5,  -- Time to wait before allowing respawn (in seconds)
+    reviveColor = "~y~",
+    respawnColor = "~r~",
+    chatColor = "~b~",
+    spawnPoints = {
+        {x = 1828.44, y = 3692.32, z = 34.22, heading = 37.12} -- Back of Sandy Shores Hospital
+    }
+}
 
-local reviveTimer = 10 -- Change the amount of time to wait before allowing revive (in seconds)
-local respawnTimer = 5 -- Change the amount of time to wait before allowing revive (in seconds)
-local reviveColor = "~y~" -- Color used for revive button
-local respawnColor = "~r~" -- Color used for respawn button
-local chatColor = "~b~" -- The chat color overall
--- Add/remove spawn points at line 77
+--- Variables ---
+local timerCount1, timerCount2, isDead, cHavePerms = Config.reviveTimer, Config.respawnTimer, false, false
 
---- Code ---
-timerCount1 = reviveTimer
-timerCount2 = respawnTimer
-isDead = false
-cHavePerms = false
-
+--- Event Handlers ---
 AddEventHandler('playerSpawned', function()
-    local src = source
-    TriggerServerEvent("RPRevive:CheckPermission", src)
+    TriggerServerEvent("RPRevive:CheckPermission", source)
 end)
 
-RegisterNetEvent("RPRevive:CheckPermission:Return")
 AddEventHandler("RPRevive:CheckPermission:Return", function(havePerms)
-	cHavePerms = havePerms
+    cHavePerms = havePerms
 end)
 
--- Turn off automatic respawn here instead of updating FiveM file.
 AddEventHandler('onClientMapStart', function()
-	Citizen.Trace("RPRevive: Disabling the autospawn.")
-	exports.spawnmanager:spawnPlayer() -- Ensure player spawns into server.
-	Citizen.Wait(2500)
-	exports.spawnmanager:setAutoSpawn(false)
-	Citizen.Trace("RPRevive: Autospawn is disabled.")
+    Citizen.Trace("RPRevive: Disabling the autospawn.")
+    exports.spawnmanager:spawnPlayer()
+    Citizen.Wait(2500)
+    exports.spawnmanager:setAutoSpawn(false)
+    Citizen.Trace("RPRevive: Autospawn is disabled.")
 end)
 
-function respawnPed(ped, coords)
-	isDead = false
-	timerCount1 = reviveTimer
-	timerCount2 = respawnTimer
-	SetEntityCoordsNoOffset(ped, coords.x, coords.y, coords.z, false, false, false, true)
-	NetworkResurrectLocalPlayer(coords.x, coords.y, coords.z, coords.heading, true, false) 
-	SetPlayerInvincible(ped, false) 
-	TriggerEvent('playerSpawned', coords.x, coords.y, coords.z, coords.heading)
-	ClearPedBloodDamage(ped)
+--- Functions ---
+local function respawnPed(ped, coords)
+    resetTimers()
+    SetEntityCoordsNoOffset(ped, coords.x, coords.y, coords.z, false, false, false, true)
+    NetworkResurrectLocalPlayer(coords.x, coords.y, coords.z, coords.heading, true, false) 
+    SetPlayerInvincible(ped, false) 
+    TriggerEvent('playerSpawned', coords.x, coords.y, coords.z, coords.heading)
+    ClearPedBloodDamage(ped)
 end
 
-function revivePed(ped)
-	isDead = false
-	timerCount1 = reviveTimer
-	timerCount2 = respawnTimer
-	local playerPos = GetEntityCoords(ped, true)
-	NetworkResurrectLocalPlayer(playerPos, true, true, false)
-	SetPlayerInvincible(ped, false)
-	ClearPedBloodDamage(ped)
+local function revivePed(ped)
+    resetTimers()
+    local playerPos = GetEntityCoords(ped, true)
+    NetworkResurrectLocalPlayer(playerPos, true, true, false)
+    SetPlayerInvincible(ped, false)
+    ClearPedBloodDamage(ped)
 end
 
-function ShowInfoRevive(text)
-	SetNotificationTextEntry("STRING")
-	AddTextComponentSubstringPlayerName(text)
-	DrawNotification(true, true)
+local function ShowInfoRevive(text)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentSubstringPlayerName(text)
+    DrawNotification(true, true)
 end
 
+local function resetTimers()
+    isDead = false
+    timerCount1 = Config.reviveTimer
+    timerCount2 = Config.respawnTimer
+end
+
+--- Threads ---
 Citizen.CreateThread(function()
-	local spawnPoints = {}
-
-	function createSpawnPoint(x, y, z, heading)
-		local newObject = {
-			x = x,
-			y = y,
-			z = z,
-			heading = heading
-		}
-
-		table.insert(spawnPoints, newObject)
-	end
-
-	createSpawnPoint(1828.44, 3692.32, 34.22, 37.12) -- Back of Sandy Shores Hospital
     while true do
-    	Citizen.Wait(0)
-		ped = GetPlayerPed(-1)
-        if IsEntityDead(ped) then
-			isDead = true
+        Citizen.Wait(0)
+        local ped = GetPlayerPed(-1)
+        if IsEntityDead(ped) and not isDead then
+            isDead = true
             SetPlayerInvincible(ped, true)
             SetEntityHealth(ped, 1)
-			ShowInfoRevive(chatColor .. 'You are dead. Use ' .. reviveColor .. 'E ' .. chatColor ..'to revive or ' .. respawnColor .. 'R ' .. chatColor .. 'to respawn.')
-            if IsControlJustReleased(0, 38) and GetLastInputMethod(0) then
+            ShowInfoRevive(Config.chatColor .. 'You are dead. Use ' .. Config.reviveColor .. 'E ' .. Config.chatColor ..'to revive or ' .. Config.respawnColor .. 'R ' .. Config.chatColor .. 'to respawn.')
+        end
+
+        if isDead then
+            if IsControlJustReleased(0, 38) and GetLastInputMethod(0) then -- E key
                 if timerCount1 == 0 or cHavePerms then
                     revivePed(ped)
-				else
-					TriggerEvent('chat:addMessage', {args = {'^1^*Wait ' .. timerCount1 .. ' more seconds before reviving'}})
-                end	
-            elseif IsControlJustReleased(0, 45) and GetLastInputMethod(0) then
+                else
+                    TriggerEvent('chat:addMessage', {args = {'^1^*Wait ' .. timerCount1 .. ' more seconds before reviving'}})
+                end 
+            elseif IsControlJustReleased(0, 45) and GetLastInputMethod(0) then -- R key
                 if timerCount2 == 0 or cHavePerms then
-					local coords = spawnPoints[math.random(1,#spawnPoints)]
-					respawnPed(ped, coords)
-				else
-					TriggerEvent('chat:addMessage', {args = {'^1^*Wait ' .. timerCount2 .. ' more seconds before respawning'}})
-				end
+                    local coords = Config.spawnPoints[math.random(#Config.spawnPoints)]
+                    respawnPed(ped, coords)
+                else
+                    TriggerEvent('chat:addMessage', {args = {'^1^*Wait ' .. timerCount2 .. ' more seconds before respawning'}})
+                end
             end
         end
     end
@@ -106,15 +97,15 @@ end)
 
 Citizen.CreateThread(function()
     while true do
+        Citizen.Wait(1000)
         if isDead then
-			if timerCount1 ~= 0 then
-				timerCount1 = timerCount1 - 1
-			end
+            if timerCount1 > 0 then
+                timerCount1 = timerCount1 - 1
+            end
 
-			if timerCount2 ~= 0 then
-				timerCount2 = timerCount2 - 1
-			end
+            if timerCount2 > 0 then
+                timerCount2 = timerCount2 - 1
+            end
         end
-        Citizen.Wait(1000)          
     end
 end)
